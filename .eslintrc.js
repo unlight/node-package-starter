b1a9c26b3a8bd1e11ec48126b82259cf750493d0
roman <s91630277@gmail.com>
2017-11-05 00:48:04 +0400

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:unicorn/recommended",
+    ],
+    "parser": "typescript-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "unicorn",
+        "typescript",
+        "import",
+    ],
+    "rules": {
+        "no-undef": 0,
+        "no-unused-vars": 0,
+        "indent": 0,
+        "import/newline-after-import": 1,
+        "import/no-duplicates": 1,
+        "import/max-dependencies": [1, { "max": 10 }],
+        "quotes": [1, "single", { "allowTemplateLiterals": true }],
+        "semi": [1, "always"],
+    }
+};
---
+++ src/index.spec.ts
@@ -0,0 +1,6 @@
+import * as assert from 'assert';
+import * as lib from './index';
+
+it('smoke test', () => {
+    assert(lib);
+});
