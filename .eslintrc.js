9db5d8a852f56c053088b964e1a6e1074baa0aa5
roman <s91630277@gmail.com>
2017-11-08 00:12:04 +0400

    Added tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
+const { rules: tslintRulesRecommended } = require('tslint/lib/configs/recommended');
+const tslintRules = Object.assign({}, tslintRulesRecommended, {
+    "member-access": false,
+    "ordered-imports": false,
+    "quotemark": false,
+    "no-var-keyword": false,
+    "object-literal-sort-keys": false,
+    "no-console": false,
+});
+
 module.exports = {
     "env": {
         "es6": true,
@@ -16,11 +26,16 @@ module.exports = {
         "unicorn",
         "typescript",
         "import",
+        "tslint",
     ],
     "rules": {
         "no-undef": 0,
         "no-unused-vars": 0,
         "indent": 0,
+        "tslint/config": [1, {
+            rules: tslintRules,
+            rulesDirectory: ["node_modules/tslint/lib/rules"],
+        }],
         "import/newline-after-import": 1,
         "import/no-duplicates": 1,
         "import/max-dependencies": [1, { "max": 10 }],
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export function hello() {
-	return 'world';
+    return 'world';
 }
