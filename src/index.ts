c9fc41f506e21a52ecde9a81ea8e60e17b34d49c
roman.vasilev <root@localhost>
2019-01-19 16:26:20 +0400

    Docs example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+/**
+ * Hello function whithout parameter
+ * @return [description]
+ */
 export function hello(): string;
 
+/**
+ * This is hello function
+  */
 export function hello(greet?: string) {
     return `${greet} world`;
 }
