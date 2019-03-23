828a511b155f4d9e252cd9e43c150d1ebfde3abb
roman.vasilev <>
2019-03-24 01:15:02 +0400

    remark config

---
+++ .remarkrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "plugins": [
+        ["toc"],
+        ["license"],
+        // ["remark-usage", {
+        //     "example": "src/example.ts",
+        //     "main": "./src/index.ts"
+        // }]
+    ]
+}
