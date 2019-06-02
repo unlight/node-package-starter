278a9060b588d745163b9aca211c38abd73809b1
roman.vasilev <>
2019-06-02 14:46:30 +0400

    WIP mutation testing

---
+++ stryker.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+    config.set({
+        maxConcurrentTestRunners: 1,
+        mutate: [
+            'src/**/*.tsx',
+            'src/**/*.ts',
+            '!src/**/*.spec.ts',
+            '!src/**/*.spec.tsx',
+            '!src/examples/**/*.*',
+        ],
+        files: [
+            'jest.config.js',
+            'src/**/*.tsx',
+            'src/**/*.ts',
+            'src/**/*.json',
+            'package.json',
+        ],
+        mutator: "typescript",
+        packageManager: "npm",
+        reporters: ["clear-text", "progress", "dashboard"],
+        testRunner: "jest",
+        transpilers: [],
+        coverageAnalysis: "off",
+        tsconfigFile: "tsconfig.json",
+    });
+};
