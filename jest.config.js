7b711135fa19513744fcb1a9285bf94430ba294b
roman.vasilev <>
2019-06-24 01:27:12 +0400

    updated jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
+    "testEnvironment": "node",
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
     },
-    collectCoverage: false,
-    coverageDirectory: 'coverage',
-    coverageReporters: [
-        // 'lcov',
-        'text',
+    "collectCoverage": false,
+    "coverageDirectory": "coverage",
+    "coverageReporters": [
+        // "lcov",
+        "text"
     ],
-    collectCoverageFrom: [
-        'src/**/*.ts',
-        '!src/**/*.spec.ts',
+    "collectCoverageFrom": [
+        "src/**/*.ts",
+        "!src/**/*.spec.ts",
     ],
-    testMatch: [
-        '<rootDir>/src/**/*.spec.ts'
+    "testMatch": [
+        "<rootDir>/src/**/*.spec.ts",
     ],
-    moduleFileExtensions: [
-        'ts',
-        'tsx',
-        'js',
-        'jsx',
-        'json',
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json"
     ],
-    modulePathIgnorePatterns: [
-        '<rootDir>/dist',
+    "modulePathIgnorePatterns": [
+        "<rootDir>/dist",
     ],
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-            isolatedModules: true,
-            tsConfig: {
-                target: 'esnext',
+    "globals": {
+        "ts-jest": {
+            "diagnostics": false,
+            "isolatedModules": true,
+            "tsConfig": {
+                "target": "esnext"
             }
-        },
-    },
+        }
+    }
 };
