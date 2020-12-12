0413f7811a92fbada68827042961a7d04fbc396f
roman <>
2020-12-12 20:06:08 +0400

    80 max length space 2

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    // "lcov",
+    'text',
+  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      isolatedModules: true,
+      tsConfig: {
+        target: 'es2017',
+      },
     },
-    collectCoverage: false,
-    coverageDirectory: 'coverage',
-    coverageReporters: [
-        // "lcov",
-        'text',
-    ],
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts'],
-    testMatch: ['<rootDir>/src/**/*.spec.ts'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-    modulePathIgnorePatterns: ['<rootDir>/dist'],
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-            isolatedModules: true,
-            tsConfig: {
-                target: 'es2017',
-            },
-        },
-    },
+  },
 };
