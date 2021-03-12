2ae09327fc4fdb6c17ec1e54e5cffbaeca9b3a64
Roman_Vasilev <>
2021-03-13 01:09:20 +0400

    chore!: Test breaking change

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,11 @@
 module.exports = {
     plugins: [
-        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+            },
+        ],
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         [
