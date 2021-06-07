e5b1e1c55b66f621f474d11aa7ea0118fb25d5d1
Roman_Vasilev <>
2021-06-07 23:08:34 +0400

    chore: update

--- .cz-config.js
+++ .cz-config.js
@@ -2,16 +2,10 @@ module.exports = {
     types: [
         { value: 'feat', name: 'feat:     A new feature' },
         { value: 'fix', name: 'fix:      A bug fix' },
-        { value: 'docs', name: 'docs:     Documentation only changes' },
-        {
-            value: 'style',
-            name:
-                'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
-        },
-        {
-            value: 'refactor',
-            name:
-                'refactor: A code change that neither fixes a bug nor adds a feature',
+        { value: 'docs', name: 'docs:     Documentation only change 'style',
+ 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+        },     value: 'refactor',
+            name: 'refactor: A code change that neither fixes a bug nor adds a feature',
         },
         {
             value: 'perf',
@@ -23,20 +17,15 @@ module.exports = {
             name: 'ci:       Changes related to continious integration',
         },
         { value: 'build', name: 'build:    Changes related to build process' },
-        {
-            value: 'chore',
-            name:
-                'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
-        },
-        { value: 'revert', name: 'revert:   Revert to a commit' },
+        {value: 'chore', 'chore:    Changes to the build process or auxiliary to libraries such as documentation generation', { value: 'revert', name: 'revert:   Revert to a commit' },
         { value: 'WIP', name: 'WIP:      Work in progress' },
     ],
 
     scopes: [{ name: 'other' }],
 
-    allowTicketNumber: true,
+    allowTicketNumber: false,
     isTicketNumberRequired: false,
-    ticketNumberPrefix: 'ISSUE-',
+    ticketNumberPrefix: '#',
     ticketNumberRegExp: '\\d{1,5}',
 
     // it needs to match the value for field type. Eg.: 'fix'
@@ -58,19 +47,16 @@ module.exports = {
         // used if allowCustomScopes is true
         customScope: 'Denote the SCOPE of this change:',
         subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-        body:
-            'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+        body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
         breaking: 'List any BREAKING CHANGES (optional):\n',
-        footer:
-            'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-        confirmCommit:
-            'Are you sure you want to proceed with the commit above?',
+        footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+        confirmCommit: 'Are you sure you want to proceed with the commit above?',
     },
 
     allowCustomScopes: true,
-    allowBreakingChanges: ['feat', 'fix'],
+    allowBreakingChanges: ['feat', 'fix', 'refactor', 'chore'],
     // skip any questions you want
-    skipQuestions: ['body', 'footer'],
+    skipQuestions: ['body'],
 
     // limit subject length
     subjectLimit: 100,
