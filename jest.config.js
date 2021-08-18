8cfc2686968d6d25724950de608bf52c5612d697
roman <Roman_Vasilev@epam.com>
2021-08-19 00:49:55 +0400

    feat: Use swc for compiling typescript

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,9 @@ const getJestMappersFromTSConfig = require('tsconfig-paths-jest-mapper');
 
 module.exports = {
     testEnvironment: 'node',
+    setupFiles: ['<rootDir>/jest.setup.ts'],
     transform: {
-        '^.+\\.tsx?$': [
-            'esbuild-jest',
-            {
-                // jsxFactory?: string
-                // jsxFragment?: string
-                // sourcemap?: boolean | 'inline' | 'external'
-                // loaders?: {
-                //   [ext: string]: Loader
-                // },
-                target: 'es2020',
-                // format?: string
-            },
-        ],
+        '^.+\\.(t|j)sx?$': '@swc/jest',
     },
     collectCoverage: false,
     coverageDirectory: 'coverage',
@@ -31,14 +20,5 @@ module.exports = {
         ...getJestMappersFromTSConfig(),
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/test/__mocks__/fileMock.js',
-    },
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-            isolatedModules: true,
-            tsconfig: {
-                target: 'es2020',
-            },
-        },
     },
 };
