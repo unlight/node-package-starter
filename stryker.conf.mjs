95e4ae8e4275a4e41f609d2369be1e70b4fe2fe7
Roman_Vasilev <>
2023-01-28 16:08:47 +0400

    chore: Update stryker.conf.mjs

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -1,28 +1,15 @@
 // Take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
-const config = {
+export default {
   packageManager: 'npm',
   commandRunner: {
-    command: 'node -r ts-node/register node_modules/mocha/bin/mocha src/**/*.spec.ts',
+    command:
+      'node -r ts-node/register node_modules/mocha/bin/mocha src/**/*.spec.ts',
   },
   coverageAnalysis: 'perTest',
-  maxConcurrentTestRunners: 1,
-  concurrency: 1,
-  mutate: [
-    'src/**/*.tsx',
-    'src/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/**/*.spec.tsx',
-    '!src/examples/**/*.*',
-  ],
-  // files: [
-  //   'jest.config.js',
-  //   'src/**/*.tsx',
-  //   'src/**/*.ts',
-  //   'src/**/*.json',
-  //   'package.json',
-  // ],
-  reporters: ['clear-text', 'progress', 'dashboard', 'html'],
+  concurrency: 2,
+  mutate: ['src/**/*.ts', '!src/**/*.spec.ts'],
+  // reporters: ['clear-text', 'progress', 'dashboard', 'html'],
+  reporters: ['clear-text', 'dots'],
   tsconfigFile: 'tsconfig.json',
 };
-export default config;
