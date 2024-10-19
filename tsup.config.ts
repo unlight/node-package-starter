18709111a566d6c42307758abe023ea81b9d0699
Roman <>
2024-10-19 15:24:31 +0400

    build: tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'], // Entry file(s)
+  target: 'es2020', // Specify ECMAScript target version
+  format: ['cjs', 'esm'], // Output formats: CommonJS and ESM
+  splitting: false, // Disable code splitting
+  sourcemap: true, // Generate sourcemaps
+  clean: true, // Clean output directory before each build
+  dts: true, // Generate declaration files,
+  minify: false,
+});
