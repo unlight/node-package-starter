3d236d88127cc830c4e0fc5c1e74cfe8faca12a5
Roman <>
2025-01-05 23:03:15 +0400

    update stryker.conf.mjs

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -2,13 +2,12 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 export default {
   packageManager: 'npm',
-  commandRunner: {
-    command: 'vitest',
-  },
+  testRunner: 'vitest',
   coverageAnalysis: 'perTest',
   concurrency: 2,
   mutate: ['src/**/*.ts', '!src/**/*.spec.ts'],
   // reporters: ['clear-text', 'progress', 'dashboard', 'html'],
   reporters: ['clear-text', 'dots'],
   tsconfigFile: 'tsconfig.json',
+  plugins: ['@stryker-mutator/vitest-runner'],
 };
