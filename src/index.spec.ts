afdcd9b481711e6908d76bf78b16fd09b2c2f7b4
Roman <>
2025-01-05 22:39:24 +0400

    build esm

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'expect';
 import { inspect } from 'util';
 
-import { hello } from './index';
+import { hello } from './index.ts';
 
 it('smoke', () => {
   expect(1).toEqual(expect.anything());
